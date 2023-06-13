120506eedc6a43d8b6d5e604cd005957ad0c0095
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-06-13 15:27:37 -0300

    feat: product API

---
+++ GeekShopping.Product.API/Models/Context/SqlServerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Product.API.Models.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext() {}
+
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
