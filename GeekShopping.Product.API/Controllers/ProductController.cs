6465af9d188ecd3471bddc468c72a2cd81d7e158
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-06-17 15:11:38 -0300

    feat: added product controller and seed migration

---
+++ GeekShopping.Product.API/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using GeekShopping.Product.API.Data.ValueObjects;
+using GeekShopping.Product.API.Models.Context;
+using GeekShopping.Product.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Product.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly SqlServerContext _context;
+        private readonly IProductRepository _repository;
+
+        public ProductController(SqlServerContext context, IProductRepository repository)
+        {
+            _context = context;
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindByIdAsync(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        {
+            if (productVO is null) return BadRequest();
+            var product = await _repository.CreateAsync(productVO);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        {
+            if (productVO is null) return BadRequest();
+            var product = await _repository.UpdateAsync(productVO);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.DeleteAsync(id);
+            return status ? Ok(status) : BadRequest();
+        }
+    }
+}
--- GeekShopping.Product.API/Migrations/20230617180641_SeedProductTable.Designer.cs
+++ GeekShopping.Product.API/Migrations/20230617180641_SeedProductTable.Designer.cs
@@ -3,6 +3,7 @@ using GeekShopping.Product.API.Models.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.Product.API.Migrations
 {
     [DbContext(typeof(SqlServerContext))]
-    partial class SqlServerContextModelSnapshot : ModelSnapshot
+    [Migration("20230617180641_SeedProductTable")]
+    partial class SeedProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
