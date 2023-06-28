1f359f05455099c66cda15b7ae366c9825ff0ccb
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-06-28 19:09:58 -0300

    feat: added crud for management products

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -12,10 +13,64 @@ namespace GeekShopping.Web.Controllers
             _productService = productService ?? throw new ArgumentNullException(nameof(productService));
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> ProductIndex()
         {
-            var products = _productService.FindAllAsync();
+            var products = await _productService.FindAllAsync();
             return View(products);
+        }
+
+        public IActionResult ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateAsync(product);
+                if (response is not null) return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var product = await _productService.FindByIdAsync(id);
+
+            if (product is not null) return View(product);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateAsync(product);
+                if (response is not null) return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var product = await _productService.FindByIdAsync(id);
+
+            if (product is not null) return View(product);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel product)
+        {
+            var response = await _productService.DeleteByIdAsync(product.Id);
+            if (response) return RedirectToAction(nameof(ProductIndex));
+
+            return View(product);
         }
     }
 }
