a986cbb54edcc856fddf7747709a193e050d59d0
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-07-03 19:08:07 -0300

    feat: added contextdb for identity project

---
+++ GeekShopping.IdentityServer/Model/SqlServerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model
+{
+    public class Context : DbContext
+    {
+        public SqlServerContext() { }
+
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
