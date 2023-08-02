2b3d254e77ba39f7148d418e62a77232500884ed
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-08-02 19:19:52 -0300

    feat: added value objects to order api

---
+++ GeekShopping.Order.API/Messages/ProductVO.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.Order.API.Messages
+{
+    public class ProductVO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+    }
+}
